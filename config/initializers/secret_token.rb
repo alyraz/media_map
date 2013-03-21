0d97ccbe91837d964abdab10948a439262948fe9
Apprentice <apprentice@dbc22.local>
2013-03-21 12:16:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mediamap::Application.config.secret_token = '6e025ef7d192b89397be7ae02e730d1978eba304bea42a716f6827659475e0003310b701740bf593dcc67d572a1f13f9d3a525e453864d751525a425b9194769'
