180f611f53e046f886fe83afd5a87b246c6261fa
Ryan Loomba <ryan.loomba@gmail.com>
2013-03-25 15:16:39 -0700

    undeleted migration

--- db/migrate/20130324215925_add_fields_to_shares_table.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsToSharesTable < ActiveRecord::Migration
-  def change
-    add_column :shares, :time, :string
-    add_column :shares, :sort_type, :string
-    add_column :shares, :category, :string
-    add_column :shares, :short_url, :string
-  end
-end
