94f0faa955eb1ce6ffb59502152308055b486182
Eno Compton 4 <eno4@ecom.com>
2013-03-24 22:42:46 -0700

    Add support for selected regions

--- app/assets/javascripts/map_controller.js
+++ app/assets/javascripts/map_controller.js
@@ -1,6 +1,4 @@
 var MapController = {
-  
-  //TODO -----> default country to wherever the user is located
   selectedCountry: 'US',
 
   selectableRegions: ["AE", "AR", "AU", "BE", "BR", "CA",
@@ -16,7 +14,7 @@ var MapController = {
   createValuesMap: function(selectableRegions){
     var values = {};
       for(var i = 0, length = selectableRegions.length; i < length; i++) {
-        values[selectableRegions[i]] = '#009999'; // OR -- #99a 
+        values[selectableRegions[i]] = '#CADFAA'; // OR -- #99a 
       }
       return values;
   },
@@ -28,45 +26,63 @@ var MapController = {
     return false;
   },
 
+  randomRegion: function(){
+    return this.selectableRegions[Math.floor(Math.random() * this.selectableRegions.length)];
+  },
+
   init: function(){
-    $('#world-map').vectorMap({
+    this.map = new jvm.WorldMap({
+      container: $('#world-map'),
+      regionsSelectable: true,
+      regionsSelectableOne: true, // allows only one selectable region
+      backgroundColor: "#a5bfdd",
+      selectedRegions: [MapController.randomRegion()],
+      regionStyle: {
+        initial: {
+          fill: "#F4F3F0",
+          stroke: 'none'
+        },
+        hover: {
+          "fill-opacity": 0.65
+        },
+        selected: {
+          fill: 'green'
+        },
+      },
+      series: {
+        regions: [{
+          values: this.createValuesMap(this.selectableRegions)
+        }]
+      },
 
-    onRegionLabelShow: function(event, label, code){
-      if(!MapController.checkIfSelectable(code)) {
-        event.preventDefault();
+      // turn off labels for unsupported countries
+      onRegionLabelShow: function(event, label, code){
+        if(!MapController.checkIfSelectable(code)) {
+          event.preventDefault();
+        }
+      },
+
+      // turn off hover state for unsupported countries
+      onRegionOver: function(event, code){
+        if(!MapController.checkIfSelectable(code)) {
+          event.preventDefault();
+        }
+      },
+
+      onRegionSelected: function(e, code, isSelected, selectedRegions){
+        // Note: This function is called twice: 
+        // 1) once when a region is *selected*; and
+        // 2) once when a region is *deselected*.
+        if(isSelected){
+          console.log("loading videos for " + code);
+          MapController.selectedCountry = code;
+          ViewController.clearMedia();
+          VideoController.retrieveVideos(code,
+                                           FormController.sortBySelection(),
+                                           FormController.timeSelection(),
+                                           FormController.categorySelection());
+        }
       }
-    },
-
-    onRegionOver: function(event, code){
-      if(!MapController.checkIfSelectable(code)) {
-        event.preventDefault();
-      }
-    },
-
-    onRegionClick: function(event, code){
-      event.preventDefault();
-      MapController.selectedCountry = code;
-      ViewController.clearMedia();
-      VideoController.retrieveVideos(code,
-                                       FormController.sortBySelection(),
-                                       FormController.timeSelection(),
-                                       FormController.categorySelection());
-    },
-
-    backgroundColor: "#0a0b2a",
-    regionStyle: {
-      initial: {
-        fill: "#aaa"
-      }
-    },
-
-    series: {
-      regions: [{
-        values: this.createValuesMap(this.selectableRegions)
-      }]
-    }
-
-
     });
   }
 };
