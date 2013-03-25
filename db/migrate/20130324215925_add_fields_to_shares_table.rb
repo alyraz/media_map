895fe9020818218ec691cb381b2ed076a87568ec
Ryan Loomba <ryan.loomba@gmail.com>
2013-03-25 15:30:48 -0700

    fixed problem in migration

--- db/migrate/20130324215925_add_fields_to_shares_table.rb
+++ db/migrate/20130324215925_add_fields_to_shares_table.rb
@@ -1,8 +1,8 @@
 class AddFieldsToSharesTable < ActiveRecord::Migration
   def change
-    add_column :shares, :frequency, :integer
-    add_column :shares, :sort_type, :integer
-    add_column :shares, :category, :integer
+    add_column :shares, :time, :string
+    add_column :shares, :sort_type, :string
+    add_column :shares, :category, :string
     add_column :shares, :short_url, :string
   end
 end
