61540239c8c863ab97af74a4d191935d3804a228
Ryan Loomba <ryan.loomba@gmail.com>
2013-03-25 00:08:58 -0700

    in progress

--- app/controllers/master_controller.rb
+++ app/controllers/master_controller.rb
@@ -1,6 +1,6 @@
 class MasterController < ApplicationController
   respond_to :json
-
+  
   def index
   end
 
@@ -11,6 +11,23 @@ class MasterController < ApplicationController
   # end
 
   def share
+    puts "--------------------------------"
     puts params
+    share = Share.new(params[:shareData])
+     if share.save
+       render :json => {data: "success"}.to_json
+     else
+      render :json => {data: "fail"}.to_json
+    end
+  end
+
+  def show
+    @share = Share.find_by_short_url(params[:short_url])
+    if @share
+      render :render :json => {:country   => @share.country,
+                               :frequency => @share.frequency,
+                               :sort_type => @share.sort_type,
+                               :category  => @share.category}.to_json
+    end
   end
 end
