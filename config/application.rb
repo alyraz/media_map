906b119d3e436a6c62c8aae0503d823e16fc919a
Alyraz <Alyssaravasio@gmail.com>
2013-03-28 15:03:47 -0700

    Disable asset initialization on precompile

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,11 @@ module Mediamap
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/app/workers)
 
+    # Disable database access while assets are compiling
+    # See: https://devcenter.heroku.com/articles/rails-asset-pipeline
+    config.assets.initialize_on_precompile = false
+
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
